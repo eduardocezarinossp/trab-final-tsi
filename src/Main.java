6147e7ac030d12a446efabfd803d8d76696a77bf
Tiago Cardoso <tiagocardoso011@gmail.com>
2020-07-13 20:22:48 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import telas.TelaBuscar;
+import telas.ProdutoBuscar;
+import telas.ProdutoCadastrar;
 import telas.Tela;
 import telas.TelaCadastroPessoa;
 import telas.TelaCadastroVenda;
@@ -10,8 +12,14 @@ public class Main {
 //		TelaCadastroVenda t = new TelaCadastroVenda();
 //		TelaCadastroVenda tela1 = new TelaCadastroVenda();
 //		Tela tela = new Tela();
+<<<<<<< Updated upstream
 //		TelaCadastro telaCadastro = new TelaCadastro();
 		TelaBuscar telaBuscar = new TelaBuscar();
+=======
+		ProdutoBuscar produto = new ProdutoBuscar();
+//		TelaCadastroPessoa telaCadastro = new TelaCadastroPessoa();
+//		TelaBuscar telaBuscar = new TelaBuscar();
+>>>>>>> Stashed changes
 	}
 	
 }
